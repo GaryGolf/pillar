8d0b379c2a71b8c666c4cd95676ac034efc4696c
GaryGolf <garygolf@mail.ru>
2017-10-11 10:47:24 +0300

    add social menu list

--- src/containers/main.tsx
+++ src/containers/main.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react'
-
+import NavMenu from '../components/nav-menu'
 interface Props {}
 interface State {}
 
 export default class Main extends React.Component <Props, State> {
   render(){
-    return <h2>Pillar</h2>
+    return <NavMenu/>
   }
 }
