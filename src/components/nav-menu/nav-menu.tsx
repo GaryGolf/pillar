fd924c273fa632f4abf88604d078cd37348b630e
GaryGolf <garygolf@mail.ru>
2017-10-11 11:12:05 +0300

    add utility bar component

--- src/components/nav-menu/nav-menu.tsx
+++ src/components/nav-menu/nav-menu.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import NavBar from './nav-bar'
-import SocialList from './social-list'
+import UtilityBar from './utility-bar'
 
 interface Props {}
 interface State {}
@@ -9,17 +9,7 @@ export default class NavMenu extends React.Component <Props, State> {
   render(){
 	return (
 	  <nav className="transition--fade transition--active" style={{minHeight: "119px"}}>
-			<div className="utility-bar bg--secondary">
-				<div className="nav-module left">
-					<span className="type--fine-print">Web Design &amp; Development Melbourne</span>
-				</div>
-				<div className="nav-module left">
-					<span className="type--fine-print">(03) 9282 7732</span>
-				</div>
-				<div className="nav-module right hidden-xs">
-					<SocialList/>
-				</div>
-			</div>
+			<UtilityBar/>
 			<NavBar/>
 	  </nav>
 	)
---
+++ src/components/nav-menu/utility-bar.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react'
+import SocialList from './social-list'
+
+interface Props {}
+
+export default (props:Props) => {
+  return (
+    <div className="utility-bar bg--secondary">
+      <div className="nav-module left">
+        <span className="type--fine-print">Web Design &amp; Development Melbourne</span>
+      </div>
+      <div className="nav-module left">
+        <span className="type--fine-print">(03) 9282 7732</span>
+      </div>
+      <div className="nav-module right hidden-xs">
+        <SocialList/>
+      </div>
+    </div>
+  )
+}
