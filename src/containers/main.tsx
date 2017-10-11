78b08f3cba3062a8cb6f8b72fad13d274077bcdf
GaryGolf <garygolf@mail.ru>
2017-10-11 17:43:43 +0300

    minor changes

--- src/containers/main.tsx
+++ src/containers/main.tsx
@@ -14,7 +14,7 @@ export default class Main extends React.Component <Props, State> {
     super(props)
     this.state = {
       showCart: false,
-      showSearch: true
+      showSearch: false
     }
   }
 
