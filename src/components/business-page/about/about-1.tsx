716b03cdc0f04a0184b46c12e205a9b0d87b87d8
GaryGolf <garygolf@mail.ru>
2017-10-12 13:55:02 +0300

    add about-1 component

--- src/components/business-page/about/about-1.tsx
+++ src/components/business-page/about/about-1.tsx
@@ -4,6 +4,25 @@ interface Props {}
 export default (props:Props) => {
   return (
     <section className="imageblock about-1 bg--secondary">
+      <div className="imageblock__content col-md-6 col-sm-4 pos-right">
+        <div className="background-image-holder" style={{background: "url(img/hero29.jpg)", opacity: 1}}>
+          <img alt="image" src="img/hero29.jpg"/>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-5 col-sm-8">
+            <h3>Assemble pages with over 150 unique content blocks</h3>
+            <p>
+                Pillar combines smart, modern styling with all the features you’ll need to launch websites of almost any kind. Achieve results faster than ever with the included Variant Page Builder.
+            </p>
+            <a className="btn btn--square" href="variant/builder.html">
+              <span className="btn__text">Launch Builder</span>
+            </a>
+          </div>
+        </div>
+        {/* <!--end of row--> */}
+      </div>
     </section>
   )
 }
--- src/components/business-page/main-content/main-container.tsx
+++ src/components/business-page/main-content/main-container.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 
 import Footer from '../../footer'
+import About1 from '../about/about-1'
 
 interface Props {}
 interface State {}
@@ -9,6 +10,7 @@ export default class MainContainer extends React.Component <Props, State> {
   render(){
     return (
       <div className="main-container transition--fade transition--active">
+        <About1/>
         <Footer/>
       </div>
     )
