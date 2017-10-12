99d4c94b18d5b730af5c31b3168b279a9ac91bc0
GaryGolf <garygolf@mail.ru>
2017-10-12 11:44:04 +0300

    add checkbox control

--- src/components/sign-up/form.tsx
+++ src/components/sign-up/form.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 interface Props {}
 interface State {
-  username: string
+  name: string
   email:  string
   password: string
   agree: boolean
@@ -11,7 +11,7 @@ export default class Form extends React.Component <Props, State > {
   constructor(props:Props){
     super(props)
     this.state = {
-      username: '',
+      name: '',
       email: '',
       password: '',
       agree: false
@@ -19,13 +19,19 @@ export default class Form extends React.Component <Props, State > {
   }
   handleFormSubmit = e => {
     e.preventDefault()
+    const {name, email, password, agree} = this.state
+    if(!name || !email || !password || !agree) return
     console.log(this.state)
   }
-  handleUsernameChange = e => {
-    const username = e.target.value
-    this.setState({username})
+  
+  handleInputChange = event => {
+    const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value
+    const name = event.target.name
+    this.setState({[name]: value})
   }
+
   render(){
+    const checkboxStyle = !this.state.agree ? 'input-checkbox' : 'input-checkbox checked'
     return (
       <div className="col-md-5 col-sm-7 bg--white text-center">
         <div className="pos-vertical-center">
@@ -34,7 +40,7 @@ export default class Form extends React.Component <Props, State > {
               A beautiful collection of <br/> hand-crafted web components
           </p>
           <div className="text-left">
-            <form method="post">
+            <form method="post" onSubmit={this.handleFormSubmit}>
               <div className="row">
                 <div className="col-sm-12">
                   <div className="input-with-icon">
@@ -43,8 +49,8 @@ export default class Form extends React.Component <Props, State > {
                     <input type="text" 
                       name="name" 
                       placeholder="Username"
-                      value={this.state.username}
-                      onChange={this.handleUsernameChange}
+                      value={this.state.name}
+                      onChange={this.handleInputChange}
                     />
                   </div>
                 </div>
@@ -54,7 +60,12 @@ export default class Form extends React.Component <Props, State > {
                   <div className="input-with-icon">
                     <label>Email Address:</label>
                     <i className="icon icon-Mail-2"></i>
-                    <input type="email" name="email" placeholder="you@yourwebsite.com"/>
+                    <input type="email" 
+                      name="email" 
+                      placeholder="you@yourwebsite.com"
+                      value={this.state.email}
+                      onChange={this.handleInputChange}
+                    />
                   </div>
                 </div>
               </div>
@@ -63,15 +74,26 @@ export default class Form extends React.Component <Props, State > {
                   <div className="input-with-icon">
                     <label>Create Password:</label>
                     <i className="icon icon-Security-Check"></i>
-                    <input type="password" name="password" placeholder="••••••"/>
+                    <input type="password" 
+                      name="password" 
+                      placeholder="••••••"
+                      value={this.state.password}
+                      onChange={this.handleInputChange}
+                    />
                   </div>
                 </div>
               </div>
               <div className="row">
                 <div className="col-sm-12 text-center">
-                  <div className="input-checkbox">
-                    <div className="inner"></div>
-                    <input type="checkbox"/>
+                  <div className={checkboxStyle}>
+                    {/* <div className="inner"/> */}
+                    <input type="checkbox"
+                      name="agree"
+                      style={{padding:'.3rem'}}
+                      className="inner"
+                      checked={this.state.agree}
+                      onChange={this.handleInputChange}
+                    />
                   </div>
                     <span className="type--fine-print">I agree to the
                       <a href="#"> terms and conditions</a>
