9baf93e76236e04758be42e01678a9c52eac47bd
GaryGolf <garygolf@mail.ru>
2017-10-12 15:47:48 +0300

    add search and cart components to business

--- src/containers/business.tsx
+++ src/containers/business.tsx
@@ -1,14 +1,56 @@
 import * as React from 'react'
-import MainContainer from '../components/business-page/main-content/main-container'
+
+import Footer from '../components/footer'
+import About from '../components/business-page/about'
+import NavBar from '../components/business-page/nav-bar'
+import Cart from '../components/cart'
+import Search from '../components/search'
 
 interface Props {}
-interface State {}
+interface State {
+  showCart: boolean
+  showSearch: boolean
+}
 
-export default class Business extends React.Component <Props, State> {
+export default class MainContainer extends React.Component <Props, State> {
+  
+  constructor(props:Props){
+    super(props)
+    this.state = {
+      showCart: false,
+      showSearch: false,
+    }
+  }
+
+  handleCartClose = () => this.setState({showCart:false})
+  
+  handleCartOpen = () => this.setState({showCart:true})
+  
+  handleSearchClose = () => this.setState({showSearch:false})
+  
+  handleSearchOpen = () => this.setState({showSearch:true})
+  
+
   render(){
+    const {showCart, showSearch} = this.state
     return (
-      <div>
-       <MainContainer/>
+      <div className="main-container transition--fade transition--active">
+        <NavBar
+          showCart={this.handleCartOpen}
+          showSearch={this.handleSearchOpen}
+        />
+        <Cart 
+          active={showCart}
+          hide={this.handleCartClose}
+        />
+        <Search
+          active={showSearch}
+          close={this.handleSearchClose}
+        />
+        <About.Second/>
+        <About.Features/>
+        <About.First/>
+        <Footer/>
       </div>
     )
   }
