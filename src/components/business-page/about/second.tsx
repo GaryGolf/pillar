5d6d126bd9524587f5f36251266ec1558eef035b
GaryGolf <garygolf@mail.ru>
2017-10-13 07:34:13 +0300

    add video controls

--- src/components/business-page/about/second.tsx
+++ src/components/business-page/about/second.tsx
@@ -1,34 +1,55 @@
 import * as React from 'react'
-interface Props {}
+import Video from './video'
 
-export default (props:Props) => {
-  return (
-    <section className="imageblock about-2 bg--dark">
-      <div className="imageblock__content col-md-6 col-sm-4 pos-left">
-        <div className="background-image-holder" style={{background:"url(img/hero28.jpg)", opacity:1}}>
-          <img alt="image" src="img/hero28.jpg"/>
+interface Props {}
+interface State {
+  showVideo: boolean
+}
+
+export default class Second extends React.Component <Props, State> {
+  constructor(props:Props){
+    super(props)
+
+    this.state = {
+      showVideo: false
+    }
+  }
+
+  handlePlayVideo = () => this.setState({showVideo:true})
+  handleStopVideo = () => this.setState({showVideo:false})
+
+  render() {
+    return (
+      <section className="imageblock about-2 bg--dark">
+        <div className="imageblock__content col-md-6 col-sm-4 pos-left">
+          <div className="background-image-holder" style={{background:"url(img/hero28.jpg)", opacity:1}}>
+            <img alt="image" src="img/hero28.jpg"/>
+          </div>
         </div>
-      </div>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-5 col-md-push-7 col-sm-8 col-sm-push-4">
-            <h2>Build smart, effective websites in no time.</h2>
-            <p>
-                Pillar features a bevy of content blocks and layouts to create websites of almost any kind — easier than ever before.
-            </p>
-            <div>
-              <div className="modal-instance modal-video-1">
-                <div className="video-play-icon video-play-icon--sm modal-trigger" data-modal-index="0"/>
-                <span className="h6">Watch Overview</span>
-                {/* <!--end of modal-container--> */}
+        <div className="container">
+          <div className="row">
+            <div className="col-md-5 col-md-push-7 col-sm-8 col-sm-push-4">
+              <h2>Build smart, effective websites in no time.</h2>
+              <p>
+                  Pillar features a bevy of content blocks and layouts to create websites of almost any kind — easier than ever before.
+              </p>
+              <div>
+                <div className="modal-instance modal-video-1">
+                  <div className="video-play-icon video-play-icon--sm modal-trigger" 
+                    data-modal-index="0"
+                    onClick={this.handlePlayVideo}
+                  />
+                  <span className="h6">Watch Overview</span>
+                </div>
               </div>
-            {/* <!--end of modal instance--> */}
             </div>
           </div>
         </div>
-      {/* <!--end of row--> */}
-      </div>
-    {/* <!--end of container--> */}
-    </section>
-  )
+        <Video
+          active={this.state.showVideo}
+          stop={this.handleStopVideo}
+        />
+      </section>
+    )
+  }
 }
