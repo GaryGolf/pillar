3838d7f76c07e110d2a7b714db6f1a6b84e8c99f
GaryGolf <garygolf@mail.ru>
2017-10-13 07:10:48 +0300

    add opacity to  slider images

--- src/components/business-page/carousel.tsx
+++ src/components/business-page/carousel.tsx
@@ -7,26 +7,26 @@ export default (props:Props) => {
   const partners = new Array(7)
     .fill(' ')
     .map((item,idx)=>(
-      <li key={idx}>
-        <img key={idx} alt="img" src={`img/partner${idx+1}.png`} />
-        </li>
+      <div key={idx} style={{opacity:.3}}>
+        <img alt="img" 
+          src={`img/partner${idx+1}.png`} 
+          style={{width:'auto'}}
+        />
+      </div>
     ))
   return (
     <section className="bg--white partners-1 space--sm">
       <div className="container">
         <div className="col-sm-12">
-          <ul className="slides owl-carousel owl-theme owl-loaded owl-text-select-on"> 
           <OwlCarousel 
-          className="owl-theme slider"
-          autoplayTimeout={2000} 
-          dots={false}
-          margin={50} 
-          items={4}
-          loop autoplay
-        >
-          {partners}
-        </OwlCarousel>
-        </ul>
+            className="owl-theme slider"
+            autoplayTimeout={2000} 
+            dots={false}
+            margin={60} 
+            autoWidth loop autoplay center
+          >
+            {partners}
+          </OwlCarousel>
         </div>
       </div>
   </section>
