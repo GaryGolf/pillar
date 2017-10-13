b99aedd507a54ea774e5fe1f7e1a01e591330d75
GaryGolf <garygolf@mail.ru>
2017-10-13 20:36:00 +0300

    add universal router

--- src/containers/main.tsx
+++ src/containers/main.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react'
-import { Link } from 'react-router-dom'
 interface Props {}
 interface State {}
+
+const Link = (props) => {
+  return <a href={props.to}>{props.children}</a>
+  
+}
+
 
 export default class Main extends React.Component <Props, State> {
   constructor(props:Props){
@@ -18,7 +23,7 @@ export default class Main extends React.Component <Props, State> {
           <Link to={'/business'}>Busines page</Link>
         </div>
         <div>
-          <Link to={'/home-shop-slider'}>Busines page</Link>
+          <Link to={'/home-shop-slider'}>Home Shop Slider</Link>
         </div>
       </div>
     )
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,27 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import {BrowserRouter as Router, Route } from 'react-router-dom'
 import store from './store'
 import Main from './containers/main'
 import Signup from './containers/signup'
 import Business from './containers/business'
 import HomeShopSlider from './containers/home-shop-slider'
+import UniversalRouter from 'universal-router'
 
-ReactDOM.render(
-  <Provider store={store} key="provider">
-    <Router>
-      <div>
-        <Route exact path="/" component={Main}/>
-        <Route path="/signup" component={Signup}/>
-        <Route path="/business" component={Business}/>
-        <Route path="/home-shop-slider" component={HomeShopSlider}/>
-      </div>
-    </Router>
-  </Provider>,document.getElementById('layout'))
+const routes = [
+  {path: '/', action: () => <Main/> },
+  {path: '/signup', action: () => <Signup/> },
+  {path: '/business', action: () => <Business/> },
+  {path: '/home-shop-slider', action: () => <HomeShopSlider/> }
+
+]
+
+const Router = new UniversalRouter(routes)
+
+Router.resolve({pathname: '/'})
+  .then(component=>{
+    ReactDOM.render(
+      <Provider store={store} key="provider">
+      {component}
+      </Provider>,document.getElementById('layout'))
+  })
