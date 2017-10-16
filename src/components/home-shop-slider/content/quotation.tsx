5ed7752b10f4aa2abd851983f234641244d46d24
GaryGolf <garygolf@mail.ru>
2017-10-16 11:35:21 +0300

    fix lint errors

--- src/components/home-shop-slider/content/quotation.tsx
+++ src/components/home-shop-slider/content/quotation.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import * as React from 'react';
 interface Props {}
 
 export default (props:Props) => {
@@ -8,11 +8,15 @@ export default (props:Props) => {
         <div className="row">
           <div className="col-md-8 col-md-offset-2 col-sm-12 text-center">
             <h6>Fall Collection 2016</h6>
-            <p className="lead">Robust design, sustainable production and honest pricing are the cornerstones of the Pillar vision — Ours values of craftsmanship and authenticity pervade every piece.</p>
+            <p className="lead">
+              Robust design, sustainable production and honest pricing are the 
+              cornerstones of the Pillar vision — Ours values of craftsmanship and 
+              authenticity pervade every piece.
+            </p>
             <img className="signature" alt="signature" src="img/signature.png"/>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
