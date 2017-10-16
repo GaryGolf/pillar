652b78f342fdf0b301a1b6552154e9f2491d1597
GaryGolf <garygolf@mail.ru>
2017-10-16 12:11:56 +0300

    fix lint errors

--- src/containers/business.tsx
+++ src/containers/business.tsx
@@ -1,39 +1,38 @@
-import * as React from 'react'
-
-import Footer from '../components/footer'
-import About from '../components/business-page/about'
-import NavBar from '../components/business-page/nav-bar'
-import Cart from '../components/cart'
-import Search from '../components/search'
-import Carousel from '../components/business-page/carousel'
+import * as React from 'react';
+import Footer from '../components/footer';
+import About from '../components/business-page/about';
+import NavBar from '../components/business-page/nav-bar';
+import Cart from '../components/cart';
+import Search from '../components/search';
+import Carousel from '../components/business-page/carousel';
 
 interface Props {}
 interface State {
-  showCart: boolean
-  showSearch: boolean
+  showCart: boolean;
+  showSearch: boolean;
 }
 
 export default class MainContainer extends React.Component <Props, State> {
   
-  constructor(props:Props){
-    super(props)
+  constructor(props:Props) {
+    super(props);
     this.state = {
       showCart: false,
       showSearch: false,
-    }
+    };
   }
 
-  handleCartClose = () => this.setState({showCart:false})
+  handleCartClose = () => this.setState({ showCart:false });
   
-  handleCartOpen = () => this.setState({showCart:true})
+  handleCartOpen = () => this.setState({ showCart:true });
   
-  handleSearchClose = () => this.setState({showSearch:false})
+  handleSearchClose = () => this.setState({ showSearch:false });
   
-  handleSearchOpen = () => this.setState({showSearch:true})
+  handleSearchOpen = () => this.setState({ showSearch:true });
   
 
   render(){
-    const {showCart, showSearch} = this.state
+    const { showCart, showSearch } = this.state;
     return (
       <div className="main-container transition--fade transition--active">
         <NavBar
@@ -54,6 +53,6 @@ export default class MainContainer extends React.Component <Props, State> {
         <Carousel/>
         <Footer/>
       </div>
-    )
+    );
   }
 }
