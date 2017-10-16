645ae736a4c2395b9ab23197e3a96b4e1daf814a
GaryGolf <garygolf@mail.ru>
2017-10-16 15:58:57 +0300

    add blog-cards-large component

---
+++ src/containers/blog-cards-large.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import NavMenu from '../components/nav-menu';
+import Footer from '../components/footer';
+import Cart from '../components/cart';
+import Search from '../components/search';
+
+interface Props {}
+interface State {
+  showCart: boolean;
+  showSearch: boolean;
+}
+
+export default class ShopProduct extends React.PureComponent <Props, State> {
+  constructor(props:Props) {
+    super(props);
+    this.state = {
+      showCart: false,
+      showSearch: false,
+    };
+  }
+
+  handleCartClose = () => this.setState({ showCart:false });
+  
+  handleCartOpen = () => this.setState({ showCart:true });
+  
+  handleSearchClose = () => this.setState({ showSearch:false });
+  
+  handleSearchOpen = () => this.setState({ showSearch:true });
+
+
+  render() {
+    const { showCart, showSearch } = this.state;
+    return (
+      <div className="main-container transition--fade transition--active">
+        <NavMenu
+          openCart={this.handleCartOpen}
+          openSearch={this.handleSearchOpen}
+        />
+        <Cart 
+          active={showCart}
+          hide={this.handleCartClose}
+        />
+        <Search
+          active={showSearch}
+          close={this.handleSearchClose}
+        />
+        <Footer/>
+      </div>
+    );
+  }
+}
