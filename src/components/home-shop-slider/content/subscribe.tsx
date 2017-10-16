b1ec4e3e80d29f3925897d2765042a2e3d657f21
GaryGolf <garygolf@mail.ru>
2017-10-16 11:40:21 +0300

    fix lint errors

--- src/components/home-shop-slider/content/subscribe.tsx
+++ src/components/home-shop-slider/content/subscribe.tsx
@@ -1,34 +1,37 @@
-import * as React from 'react'
+import * as React from 'react';
 interface Props {}
 interface State {
-  email: string
+  email: string;
 }
 
 export default class Subscribe extends React.Component <Props, State> {
 
-  constructor(props:Props){
-    super(props)
-
+  constructor(props:Props) {
+    super(props);
     this.state = {
-      email: ''
-    }
+      email: '',
+    };
   }
 
-  handleInput = event => {
-    const email = event.target.value
-    this.setState({email})
+  handleInput = (event) => {
+    const email = event.target.value;
+    this.setState({ email });
   }
-  handleSubscribe = event => {
-    const {email} = this.state
-    event.preventDefault()
-    if(!email) return
-    console.log('Email:',email)
+  handleSubscribe = (event) => {
+    const { email } = this.state;
+    event.preventDefault();
+    if (!email) return;
+    console.log('Email:',email);
   }
   render() {
-    const {email} = this.state
+    const { email } = this.state;
+    const dataError = 'Please fill all fields correctly.';
+    const dataSuccess = 'Thanks for signing up! Please check your inbox for ' +
+      'confirmation email.';
     return (
       <section className="subscribe subscribe-1 space--sm imagebg" data-overlay="3">
-        <div className="background-image-holder" style={{background:"url(img/header4.jpg)",opacity:1}}>
+        <div className="background-image-holder" 
+          style={{ background:'url(img/header4.jpg)',opacity:1 }}>
           <img alt="image" src="img/header4.jpg"/>
         </div>
         <div className="container">
@@ -39,11 +42,16 @@ export default class Subscribe extends React.Component <Props, State> {
               </div>
             </div>
             <div className="col-sm-6">
-              {/* <form className="form--merge form--no-labels form--active" action="http://mrareco.createsend.com/t/d/s/kieth/?callback=?" method="post" id="subForm" data-error="Please fill all fields correctly." data-success="Thanks for signing up! Please check your inbox for confirmation email." noValidate> */}
+              {/* <form className="form--merge form--no-labels form--active" 
+              action="http://mrareco.createsend.com/t/d/s/kieth/?callback=?" 
+              method="post" id="subForm" data-error="Please fill all fields correctly." 
+              data-success="Thanks for signing up! Please check your inbox for confirmation email." 
+              noValidate> */}
               <form className="form--merge form--no-labels form--active"  
                 onSubmit={this.handleSubscribe}
-                data-error="Please fill all fields correctly." 
-                data-success="Thanks for signing up! Please check your inbox for confirmation email." noValidate>
+                data-error={dataError}
+                data-success={dataSuccess}
+                noValidate>
                 <br/>
                 <input type="email"
                   id="fieldEmail" 
@@ -60,6 +68,6 @@ export default class Subscribe extends React.Component <Props, State> {
           </div>
       </div>
     </section>
-    )
+    );
   }
 }
