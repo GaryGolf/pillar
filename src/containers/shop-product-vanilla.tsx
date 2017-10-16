5baf569e45b2fe01af427441c54250f57e4f7b33
GaryGolf <garygolf@mail.ru>
2017-10-16 14:18:31 +0300

    create shop-product-vanulla component

---
+++ src/containers/shop-product-vanilla.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+interface Props {}
+interface State {}
+
+export default class ShopProduct extends React.PureComponent <Props, State> {
+  constructor(props:Props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
