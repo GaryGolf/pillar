2b45378fdbb0c9665a412d82d6c090ffd78990ef
GaryGolf <garygolf@mail.ru>
2017-10-16 11:53:00 +0300

    fix lint errors

--- src/components/business-page/nav-bar/nav-menu.tsx
+++ src/components/business-page/nav-bar/nav-menu.tsx
@@ -1,55 +1,59 @@
-import * as React from 'react'
-import Logo from './logo-module'
-import NavBar from '../../nav-menu/nav-bar'
+import * as React from 'react';
+import Logo from './logo-module';
+import NavBar from '../../nav-menu/nav-bar';
 interface Props {
-  showCart():void
-  showSearch():void
+  showCart():void;
+  showSearch():void;
 }
 interface State {
-  open: boolean
-  fixMenu: boolean
+  open: boolean;
+  fixMenu: boolean;
 }
 
 export default class NavMenu extends React.Component <Props, State> {
-  constructor(props:Props){
-    super(props)
-    this.state= {
+  constructor(props:Props) {
+    super(props);
+    this.state = {
       open:false,
       fixMenu: false,
-    }
-    this.handleScroll = this.handleScroll.bind(this)
-    window.addEventListener('scroll', this.handleScroll)
+    };
+    this.handleScroll = this.handleScroll.bind(this);
   }
 
-  componentWillUnmount(){
-    window.removeEventListener('screen', this.handleScroll)
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
   }
 
-  handleScroll(){
-    const {fixMenu} = this.state
-    if(!fixMenu && document.documentElement.scrollTop > 200) {
+  componentWillUnmount() {
+    window.removeEventListener('screen', this.handleScroll);
+  }
 
-      this.setState({fixMenu:true})
+  handleScroll() {
+    const { fixMenu } = this.state;
+    if (!fixMenu && document.documentElement.scrollTop > 200) {
+
+      this.setState({ fixMenu:true });
 
     } else if (fixMenu && document.documentElement.scrollTop <= 200) {
 
-      this.setState({fixMenu:false})
+      this.setState({ fixMenu:false });
 
     }
   }
-  handleNavOpenToggle = e => {
-		this.setState(state=>({open:!state.open}))
-	}
 
-  render(){
-    const {open, fixMenu} = this.state
-    const {showCart, showSearch} = this.props
-		const navStyle = !open
-			?	"transition--fade transition--active"
-      : "transition--fade transition--active nav-open"
+  handleNavOpenToggle = (event) => {
+    this.setState(state => ({ open:!state.open }));
+  }
+
+  render() {
+    const { open, fixMenu } = this.state;
+    const { showCart, showSearch } = this.props;
+    const navStyle = !open
+			?	'transition--fade transition--active'
+      : 'transition--fade transition--active nav-open';
       
     return (
-      <nav className={navStyle} style={{minHeight:"164px"}}>
+      <nav className={navStyle} style={{ minHeight:'164px' }}>
         <div className="container nav-stack">
           <Logo/>
           <div className="row">
@@ -67,6 +71,6 @@ export default class NavMenu extends React.Component <Props, State> {
           </div>
         </div>
       </nav>
-    )
+    );
   }
 }
--- src/components/nav-menu/social-list.tsx
+++ src/components/nav-menu/social-list.tsx
@@ -1,35 +1,35 @@
-import * as React from 'react'
+import * as React from 'react';
 
 interface Props {}
 
 export default (props:Props) => {
   return(
-  <ul className="social-list">
-    <li>
-      <a href="#">
-        <i className="socicon-twitter"></i>
-      </a>
-    </li>
-    <li>
-      <a href="#">
-        <i className="socicon-dribbble"></i>
-      </a>
-    </li>
-    <li>
-      <a href="#">
-        <i className="socicon-facebook"></i>
-      </a>
-    </li>
-    <li>
-      <a href="#">
-        <i className="socicon-instagram"></i>
-      </a>
-    </li>
-    <li>
-      <a href="#">
-        <i className="socicon-pinterest"></i>
-      </a>
-    </li>
-  </ul>
-  )
-}
+    <ul className="social-list">
+      <li>
+        <a href="#">
+          <i className="socicon-twitter"></i>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i className="socicon-dribbble"></i>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i className="socicon-facebook"></i>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i className="socicon-instagram"></i>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i className="socicon-pinterest"></i>
+        </a>
+      </li>
+    </ul>
+  );
+};
