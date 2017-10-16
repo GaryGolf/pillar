445ec352a3d040d85f0189f9f04faa735bbae739
GaryGolf <garygolf@mail.ru>
2017-10-16 10:55:29 +0300

    fix litn errors

--- src/components/nav-menu/menu/home.tsx
+++ src/components/nav-menu/menu/home.tsx
@@ -1,18 +1,18 @@
-import * as React from 'react'
+import * as React from 'react';
 interface Props {
-  active:boolean
-  toggle(name:string):void
+  active: boolean;
+  toggle(name:string): void;
 }
 
 export default (props:Props) => {
 
-  const activeStyle = props.active ? {className: 'active'} : null
+  const activeStyle = props.active ? { className: 'active' } : null;
 
   const toggleMenu = (event) => {
-    event.preventDefault()
-    if(!props.active) props.toggle('home')
-    else props.toggle('')
-  }
+    event.preventDefault();
+    if (!props.active) props.toggle('home');
+    else props.toggle('');
+  };
 
   return (
     <li {...activeStyle}>
@@ -120,5 +120,5 @@ export default (props:Props) => {
         </li>
       </ul>
     </li>
-  )
-}
+  );
+};
