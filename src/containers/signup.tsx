8cc9b2647cb9a905d9410b939925b0c7b2a12932
GaryGolf <garygolf@mail.ru>
2017-10-16 11:59:18 +0300

    fix lint errors

--- src/containers/signup.tsx
+++ src/containers/signup.tsx
@@ -1,59 +1,61 @@
-import * as React from 'react'
-import NavMenu from '../components/nav-menu'
-import SignUp from '../components/sign-up/main-container'
-import Cart from '../components/cart'
-import Search from '../components/search'
+import * as React from 'react';
+import NavMenu from '../components/nav-menu';
+import SignUp from '../components/sign-up/main-container';
+import Cart from '../components/cart';
+import Search from '../components/search';
 
 interface Props {}
 interface State {
-  showCart: boolean
-  showSearch: boolean
-  fixMenu: boolean
+  showCart: boolean;
+  showSearch: boolean;
+  fixMenu: boolean;
 }
 
 export default class Signup extends React.Component <Props, State> {
 
-  constructor(props:Props){
-    super(props)
+  constructor(props:Props) {
+    super(props);
     this.state = {
       showCart: false,
       showSearch: false,
-      fixMenu: false
-    }
-    this.handleScroll = this.handleScroll.bind(this)
-    window.addEventListener('scroll', this.handleScroll)
+      fixMenu: false,
+    };
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
   }
   componentWillUnmount(){
-    window.removeEventListener('screen', this.handleScroll)
+    window.removeEventListener('screen', this.handleScroll);
   }
   handleCartClose = () => {
-    this.setState({showCart:false})
+    this.setState({ showCart:false });
   }
   handleCartOpen = () => {
-    this.setState({showCart:true})
+    this.setState({ showCart:true });
   }
   handleSearchClose = () => {
-    this.setState({showSearch:false})
+    this.setState({ showSearch:false });
   }
   handleSearchOpen = () => {
-    this.setState({showSearch:true})
+    this.setState({ showSearch:true });
   }
 
-  handleScroll(){
-    const {fixMenu} = this.state
-    if(!fixMenu && document.documentElement.scrollTop > 200) {
+  handleScroll() {
+    const { fixMenu } = this.state;
+    if (!fixMenu && document.documentElement.scrollTop > 200) {
 
-      this.setState({fixMenu:true})
+      this.setState({ fixMenu:true });
 
     } else if (fixMenu && document.documentElement.scrollTop <= 200) {
 
-      this.setState({fixMenu:false})
+      this.setState({ fixMenu:false });
 
     }
   }
 
   render(){
-    const {showCart, showSearch, fixMenu} = this.state
+    const { showCart, showSearch, fixMenu } = this.state;
     return (
       <div>
         <NavMenu 
@@ -71,6 +73,6 @@ export default class Signup extends React.Component <Props, State> {
         />
         <SignUp/>
       </div>
-    )
+    );
   }
 }
