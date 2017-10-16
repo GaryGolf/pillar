d207b5409535e1e1d8e17bf12ae268db7976e59d
GaryGolf <garygolf@mail.ru>
2017-10-16 12:04:16 +0300

    fix lint errors

--- src/components/sign-up/image.tsx
+++ src/components/sign-up/image.tsx
@@ -1,16 +1,16 @@
-import * as React from 'react'
+import * as React from 'react';
 interface Props {}
 
 export default (props:Props) => {
   const style = {
-    background: "url('img/hero24.jpg')",
-    opacity: 1
-  }
+    background: 'url(\'img/hero24.jpg\')',
+    opacity: 1,
+  };
   return (
     <div className="col-md-7 col-sm-5">
       <div className="background-image-holder" style={style}>
         <img alt="image" src="img/hero24.jpg"/>
       </div>
     </div>
-  )
-}
+  );
+};
