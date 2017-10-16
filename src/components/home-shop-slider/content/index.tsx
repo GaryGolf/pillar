d1130bfb37bbf5ce7611b17055d76608660e901d
GaryGolf <garygolf@mail.ru>
2017-10-16 11:41:14 +0300

    fix lint errors

--- src/components/home-shop-slider/content/index.tsx
+++ src/components/home-shop-slider/content/index.tsx
@@ -1,6 +1,6 @@
-import Quotation from './quotation'
-import Collection from './collection'
-import Features from './features'
-import Subscribe from './subscribe'
+import Quotation from './quotation';
+import Collection from './collection';
+import Features from './features';
+import Subscribe from './subscribe';
 
-export default {Quotation, Collection, Features, Subscribe}
+export default { Quotation, Collection, Features, Subscribe };
