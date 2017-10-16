dcd29396e98852ecdd39254297a09af84fc91289
GaryGolf <garygolf@mail.ru>
2017-10-16 11:08:38 +0300

    fix litn errors

--- src/components/nav-menu/menu/pages.tsx
+++ src/components/nav-menu/menu/pages.tsx
@@ -1,17 +1,17 @@
-import * as React from 'react'
+import * as React from 'react';
 interface Props {
-  active:boolean
-  toggle(name:string):void
+  active: boolean;
+  toggle(name:string): void;
 }
 
 export default (props:Props) => {
-  const activeStyle = props.active ? {className: "active"} : null
+  const activeStyle = props.active ? { className: 'active' } : null;
   
-  const toggleMenu = e => {
-    e.preventDefault()
-    if(!props.active) props.toggle('pages')
-    else props.toggle('')
-  }
+  const toggleMenu = (event) => {
+    event.preventDefault();
+    if (!props.active) props.toggle('pages');
+    else props.toggle('');
+  };
   return (
     <li {...activeStyle}>
       <a href="#" onClick={toggleMenu}>Pages</a>
@@ -94,5 +94,5 @@ export default (props:Props) => {
         </li>
       </ul>
     </li>
-  )
-}
+  );
+};
