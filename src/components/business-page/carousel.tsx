51661f5ba44ff8f09b80b4f34c6ffb306ef186cd
GaryGolf <garygolf@mail.ru>
2017-10-16 11:30:32 +0300

    fix lint errors

--- src/components/business-page/carousel.tsx
+++ src/components/business-page/carousel.tsx
@@ -1,19 +1,21 @@
-import * as React from 'react'
-import OwlCarousel from 'react-owl-carousel'
+import * as React from 'react';
+import OwlCarousel from 'react-owl-carousel';
 
 interface Props {}
 
 export default (props:Props) => {
-  const partners = new Array(7)
-    .fill(' ')
-    .map((item,idx)=>(
-      <div key={idx} style={{opacity:.3}}>
+  const partners: JSX.Element[] = [];
+  partners.length = 7;
+  partners  
+    .fill(<div/>)
+    .map((item,idx) => (
+      <div key={idx} style={{ opacity:.3 }}>
         <img alt="img" 
-          src={`img/partner${idx+1}.png`} 
-          style={{width:'auto'}}
+          src={`img/partner${idx + 1}.png`} 
+          style={{ width:'auto' }}
         />
       </div>
-    ))
+    ));
   return (
     <section className="bg--white partners-1 space--sm">
       <div className="container">
@@ -30,5 +32,5 @@ export default (props:Props) => {
         </div>
       </div>
   </section>
-  )
-}
+  );
+};
