6f084b9dbdafb3268525c8f3d5db0c0e63173af6
GaryGolf <garygolf@mail.ru>
2017-10-16 11:16:34 +0300

    fix litn errors

--- src/components/home-shop-slider/content/collection.tsx
+++ src/components/home-shop-slider/content/collection.tsx
@@ -1,12 +1,12 @@
-import * as React from 'react'
+import * as React from 'react';
 interface Props {}
 
 interface Product {
-  name: string
-  photo: string
-  price: string
-  last_price?: string
-  filter: 'Accessories' | 'apparel'
+  name: string;
+  photo: string;
+  price: string;
+  last_price?: string;
+  filter: 'Accessories' | 'apparel';
 }
 
 export default (props:Props) => {
@@ -16,47 +16,51 @@ export default (props:Props) => {
       photo: 'img/product-large-5.jpg',
       price: '$299.00',
       last_price: '$329.00',
-      filter: 'Accessories' 
+      filter: 'Accessories',
     },
     {
       name: 'Dave Wool Beanie',
       photo: 'img/product-large-3.jpg',
       price: '$34.00',
-      filter: 'apparel' 
+      filter: 'apparel',
     },
     {
       name: 'Earl Sweatshirt',
       photo: 'img/product-large-2.jpg',
       price: '$49.00',
       last_price: '$89.00',
-      filter: 'Accessories' 
+      filter: 'Accessories',
     },
     {
       name: 'Logan Slub Tee',
       photo: 'img/product-large-8.jpg',
       price: '$59.00',
-      filter: 'apparel' 
+      filter: 'apparel',
     },
     {
       name: 'Hunter Formal Shirt',
       photo: 'img/product-large-9.jpg',
       price: '$109.99',
-      filter: 'apparel' 
+      filter: 'apparel',
     },
     {
       name: 'Kenneth Leather Wallet',
       photo: 'img/product-large-10.jpg',
       price: '$75.00',
-      filter: 'Accessories' 
+      filter: 'Accessories',
     },
-  ]
+  ];
   const shop = products.map(item => (
-    <div key={item.name} className={`col-md-4 col-sm-6 masonry__item filter-${item.filter}`}
+    <div key={item.name} 
+      className={`col-md-4 col-sm-6 masonry__item filter-${item.filter}`}
       data-masonry-filter={item.filter}>
       <a href="#">
         <div className="shop-item shop-item-1">
           <div className="shop-item__price hover--reveal">
-            {!item.last_price?null:<span className="type--strikethrough">{item.last_price}</span>}
+            {
+              !item.last_price ? null 
+              : <span className="type--strikethrough">{item.last_price}</span>
+            }
             <span>{item.price}</span>
           </div>
           <div className="shop-item__image">
@@ -68,18 +72,19 @@ export default (props:Props) => {
         </div>
       </a>
     </div>
-  ))
+  ));
+
   return (
     <section className="masonry-contained shop-home">
       <div className="container">
         <div className="row">
           <div className="masonry masonry-shop">
-            <div className="masonry__container masonry--animate masonry--active" style={{position:"relative",height:"842px"}}>
+            <div className="masonry__container masonry--animate masonry--active" 
+              style={{ position:'relative',height:'842px' }}>
               {shop}
             </div>
           </div>
         </div>
-        {/* <!--end of row--> */}
         <div className="row">
           <div className="col-sm-12 text-center">
             <a className="btn" href="#">
@@ -89,9 +94,7 @@ export default (props:Props) => {
             </a>
           </div>
         </div>
-        {/* <!--end of row--> */}
-    </div>
-    {/* <!--end of container--> */}
-</section>
-  )
-}
+      </div>
+    </section>
+  );
+};
