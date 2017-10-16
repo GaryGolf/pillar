aba1eef8152fddabd244d0d661cb71797084cd07
GaryGolf <garygolf@mail.ru>
2017-10-16 10:32:42 +0300

    fix tslint errors

--- src/components/business-page/about/second.tsx
+++ src/components/business-page/about/second.tsx
@@ -1,28 +1,28 @@
-import * as React from 'react'
-import Video from './video'
+import * as React from 'react';
+import Video from './video';
 
 interface Props {}
 interface State {
-  showVideo: boolean
+  showVideo: boolean;
 }
 
 export default class Second extends React.Component <Props, State> {
-  constructor(props:Props){
-    super(props)
-
+  constructor(props: Props) {
+    super(props);
     this.state = {
-      showVideo: false
-    }
+      showVideo: false,
+    };
   }
 
-  handlePlayVideo = () => this.setState({showVideo:true})
-  handleStopVideo = () => this.setState({showVideo:false})
+  handlePlayVideo = () => this.setState({ showVideo: true });
+  handleStopVideo = () => this.setState({ showVideo: false });
 
   render() {
     return (
       <section className="imageblock about-2 bg--dark">
         <div className="imageblock__content col-md-6 col-sm-4 pos-left">
-          <div className="background-image-holder" style={{background:"url(img/hero28.jpg)", opacity:1}}>
+          <div className="background-image-holder" 
+            style={{ background: 'url(img/hero28.jpg)', opacity:1 }}>
             <img alt="image" src="img/hero28.jpg"/>
           </div>
         </div>
@@ -31,7 +31,8 @@ export default class Second extends React.Component <Props, State> {
             <div className="col-md-5 col-md-push-7 col-sm-8 col-sm-push-4">
               <h2>Build smart, effective websites in no time.</h2>
               <p>
-                  Pillar features a bevy of content blocks and layouts to create websites of almost any kind — easier than ever before.
+                  Pillar features a bevy of content blocks and layouts 
+                  to create websites of almost any kind — easier than ever before.
               </p>
               <div>
                 <div className="modal-instance modal-video-1">
@@ -50,6 +51,6 @@ export default class Second extends React.Component <Props, State> {
           stop={this.handleStopVideo}
         />
       </section>
-    )
+    );
   }
 }
