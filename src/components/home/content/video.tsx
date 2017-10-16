df24f68d23466ee06dc21e2618075eeca6f54cb9
GaryGolf <garygolf@mail.ru>
2017-10-16 10:19:50 +0300

    rebuild video component

--- src/components/home/content/video.tsx
+++ src/components/home/content/video.tsx
@@ -1,23 +1,51 @@
 import * as React from 'react';
+import { stringify } from 'qs';
 
-import * as styles from './video.css'
+import * as styles from './video.css';
 interface Props {}
 
 export default (props:Props) => {
-  const url = 'https://www.youtube.com/embed/JyNIJ8U03I0?autoplay=1&mute=1&modestbranding=1'+
-    '&controls=0&showinfo=0&rel=0&enablejsapi=1&version=3&playerapiid=mbYTP_video_1508003128161'+
-    '&origin=http%3A%2F%2Fpillar.mediumra.re&allowfullscreen=true&wmode=transparent&iv_load_policy=3'+
-    '&html5=1&widgetid=1';
+
+  const params = {
+    autoplay: 1,
+    mute:   1,
+    modestbranding: 1,
+    controls: 0,
+    showinfo: 0,
+    rel:  0,
+    enablejsapi:  1,
+    version:  3,
+    playerapiid:  'mbYTP_video_1508003128161',
+    origin: document.location.origin,
+    allowfullscreen:  true,
+    wmode:  'transparent',
+    iv_load_policy: 3,
+    html5:  1,
+    widgetid: 1,
+  };
+
+  const url = `https://www.youtube.com/embed/JyNIJ8U03I0?${stringify(params)}`;
+  const dataProperty = '{videoURL:&quot;JyNIJ8U03I0&quot;,containment:&quot;self&quot;,' +
+    'autoPlay:true, mute:true, startAt:0, opacity:1}';
 
   return (
     <section className="imagebg videobg height-100 video-active" data-overlay="5">
-      
-      <iframe 
-        className={styles.video}
-        src={url}
-        frameBorder={0}
-        allowFullScreen
-      />
+
+      <div id="video_1508003128161"
+        className="youtube-background mb_YTPlayer isMuted" 
+        data-video-url="JyNIJ8U03I0" 
+        data-start-at="0"
+        data-property={dataProperty}
+        style={{ backgroundImage:'none' }}>
+
+        <iframe 
+          className={styles.video}
+          src={url}
+          frameBorder={0}
+          allowFullScreen
+        />
+
+      </div>
 
       <div className="background-image-holder" 
         style={{ background:'url(img/hero27.jpg)', opacity:1 }}>
