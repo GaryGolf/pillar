2105ec9b660ec321940e5f3e3b9efd3fb1a0307b
GaryGolf <garygolf@mail.ru>
2017-10-16 21:19:16 +0300

    add blog-post-container component

---
+++ src/components/blog-post-image-header/blog-post-container.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+interface Props {}
+interface State {}
+
+export default class BlogPostContainer extends React.PureComponent <Props, State> {
+  constructor(props:Props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <section className="blog-post">
+     
+      </section>
+    );
+  }
+}
