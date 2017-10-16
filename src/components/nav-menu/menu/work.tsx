bdd95c037413e9f0f79b70979337e57eb263c286
GaryGolf <garygolf@mail.ru>
2017-10-16 10:56:35 +0300

    fix litn errors

--- src/components/nav-menu/menu/work.tsx
+++ src/components/nav-menu/menu/work.tsx
@@ -1,16 +1,16 @@
 import * as React from 'react';
 
 interface Props {
-  active:boolean
-  toggle(name:string):void
+  active: boolean;
+  toggle(name:string): void;
 };
 
 export default (props:Props) => {
-  const activeStyle = props.active ? {className: 'active'} : null;
+  const activeStyle = props.active ? { className: 'active' } : null;
   
   const toggleMenu = (event) => {
     event.preventDefault();
-    if(!props.active) props.toggle('work');
+    if (!props.active) props.toggle('work');
     else props.toggle('');
   };
 
