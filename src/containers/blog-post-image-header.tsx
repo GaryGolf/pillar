081357bccd1f7550a99490fbff8f9e700d66dac6
GaryGolf <garygolf@mail.ru>
2017-10-16 21:09:12 +0300

    add blog-post-image-header component

---
+++ src/containers/blog-post-image-header.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import Cart from '../components/cart';
+import Search from '../components/search';
+import Footer from '../components/footer';
+import NavBar from '../components/home/nav-bar';
+
+interface Props {}
+interface State {
+  showCart: boolean;
+  showSearch: boolean;
+}
+
+export default class HomeShopSlider extends React.Component <Props, State> {
+  constructor(props:Props) {
+    super(props);
+    this.state = {
+      showCart: false,
+      showSearch: false,
+    };
+  }
+
+  handleCartClose = () => this.setState({ showCart:false });
+  
+  handleCartOpen = () => this.setState({ showCart:true });
+  
+  handleSearchClose = () => this.setState({ showSearch:false });
+  
+  handleSearchOpen = () => this.setState({ showSearch:true });
+
+  render() {
+
+    const { showCart, showSearch } = this.state;
+
+    return (
+      <div className="main-container transition--fade transition--active">
+        <NavBar
+          showCart={this.handleCartOpen}
+          showSearch={this.handleSearchOpen}
+        />
+        <Cart 
+          active={showCart}
+          hide={this.handleCartClose}
+        />
+        <Search
+          active={showSearch}
+          close={this.handleSearchClose}
+        />
+  
+        <Footer/>           
+      </div>
+    );
+  }
+}
