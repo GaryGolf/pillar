2a852a1c3ecc11a118c14b5e2952170c603c8207
GaryGolf <garygolf@mail.ru>
2017-10-21 14:51:22 +0300

    fix lint errors

--- src/api/app-config.ts
+++ src/api/app-config.ts
@@ -1,15 +1,15 @@
 const appConfig: AppConfig = {
-   ecommerce: {
-      currencyCode: 'GBP'
-   },
-   social: {
-      FacebookURL: 'https://facebook.com/theme',
-      TwitterURL: 'https://twitter.com/theme',
-      InstagramURL: 'https://instagram.com/theme'
-   },
-   theme: {
-      primaryColor: '#425cbb'
-   }
-}
+  ecommerce: {
+    currencyCode: 'GBP',
+  },
+  social: {
+    FacebookURL: 'https://facebook.com/theme',
+    TwitterURL: 'https://twitter.com/theme',
+    InstagramURL: 'https://instagram.com/theme',
+  },
+  theme: {
+    primaryColor: '#425cbb',
+  },
+};
 
 export default appConfig;
