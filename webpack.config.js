32100054f0d6b11f6a79b7d31a7eb4d91236557b
GaryGolf <garygolf@mail.ru>
2017-10-21 06:43:36 +0300

    minor changes

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 const sourcePath = path.join(__dirname, './src')
 const PRODUCTION = process.argv.indexOf('-p') >= 0
-const outPath = PRODUCTION ? path.join(__dirname, '../api/webroot') : path.join(__dirname, './dist')
+const outPath = path.join(__dirname, './dist')
 
 module.exports = {
   context: sourcePath,
@@ -62,7 +62,6 @@ module.exports = {
       },
       // static assets 
       { test: /\.json$/, use: 'json-loader' },
-      { test: /\.html$/, use: 'html-loader' },
       { test: /\.png$/, use: 'url-loader?limit=10000' },
       { test: /\.jpg$/, use: 'file-loader' }
     ],
