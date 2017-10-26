84d3e172b48fd12acdaa8d741fc32922d7198ef4
GaryGolf <garygolf@mail.ru>
2017-10-26 18:07:35 +0300

    add parallax effect to video landing-app component

--- src/components/home-landing-app/landing-app-video.tsx
+++ src/components/home-landing-app/landing-app-video.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-
+import { Parallax } from 'react-parallax';
 interface Props {}
 interface State {
   showVideo: boolean;
@@ -43,31 +43,25 @@ export default class LandingAppVideo extends React.PureComponent <Props, State> 
     </div>
     );
 
-    const imageHolderStyle = {
-      transform:'translate3d(0px, 10px, 0px)', 
-      background:'url(/img/project-single-4.jpg)', 
-      opacity: 1,
-    };
     return (
-      <section className="imagebg section--even parallax" data-overlay="2">
-        <div className="background-image-holder" style={imageHolderStyle}>
-          <img alt="background" src="img/project-single-4.jpg"/>
-        </div>
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-12 text-center">
-              <div className="modal-instance modal-video-1">
-                <div className="video-play-icon  modal-trigger" 
-                  data-modal-index="0"
-                  onClick={this.handlePlay}
-                />
-                <span className="h6">How Does Pillar Work?</span>
+      <Parallax bgImage="/img/project-single-4.jpg" strength={200}>
+        <section className="imagebg section--even parallax" data-overlay="2">
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-12 text-center">
+                <div className="modal-instance modal-video-1">
+                  <div className="video-play-icon  modal-trigger" 
+                    data-modal-index="0"
+                    onClick={this.handlePlay}
+                  />
+                  <span className="h6">How Does Pillar Work?</span>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        {video}
-      </section>
+          {video}
+        </section>
+      </Parallax>
     );
   }
 }
