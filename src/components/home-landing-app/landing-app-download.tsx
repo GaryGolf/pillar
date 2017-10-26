c1b5ede0e6143c31640e1111847b45ce3d805ee9
GaryGolf <garygolf@mail.ru>
2017-10-26 18:00:23 +0300

    ad parallax component

--- src/components/home-landing-app/landing-app-download.tsx
+++ src/components/home-landing-app/landing-app-download.tsx
@@ -1,36 +1,30 @@
 import * as React from 'react';
+import { Parallax, Background } from 'react-parallax';
 
 interface Props {}
 
 export default (props:Props) => {
   
-  const imageHolderStyle = {
-    transform: 'translate3d(0px, 185.5px, 0px)',
-    background: 'url(/img/hero4.jpg)',
-    opacity: 1,
-  };
-
   return (
-    <section className="imagebg section--even parallax" data-overlay="5">
-      <div className="background-image-holder" style={imageHolderStyle}>
-        <img alt="image" src="/img/hero4.jpg"/>
-      </div>
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-10 col-sm-offset-1 text-center">
-            <a className="btn" href="#">
-              <span className="btn__text">
-                <i className="socicon-apple"></i>&nbsp; Download for iOS
-              </span>
-            </a>
-            <a className="btn" href="#">
-              <span className="btn__text">
-                <i className="socicon-play"></i>&nbsp; Download for Android
-              </span>
-            </a>
+    <Parallax bgImage="/img/hero4.jpg" strength={200}> 
+      <section className="imagebg section--even parallax" data-overlay="5">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-10 col-sm-offset-1 text-center">
+              <a className="btn" href="#">
+                <span className="btn__text">
+                  <i className="socicon-apple"></i>&nbsp; Download for iOS
+                </span>
+              </a>
+              <a className="btn" href="#">
+                <span className="btn__text">
+                  <i className="socicon-play"></i>&nbsp; Download for Android
+                </span>
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </Parallax>
   );
 };
