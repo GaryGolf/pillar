2f730cf4f9ed4d53558f5f9f9902e199c3f8c63c
GaryGolf <garygolf@mail.ru>
2017-10-28 20:06:59 +0300

    add youtube-modal component to home-business-startup

--- src/components/home-business-startup/home-business-startup-image.tsx
+++ src/components/home-business-startup/home-business-startup-image.tsx
@@ -1,46 +1,74 @@
 import * as React from 'react';
-interface Props {}
+import YouTubeModal from 'components/common/youtube-modal';
 
-export default (prop:Props) => {
-  const imageHolderStyle = {
-    transform: 'translate3d(0px, 12.5px, 0px)', 
-    background: 'url(img/hero7.jpg)', 
-    opacity: 1,
-    top: 0, 
-  };
-  return (
-    <section className="height-100 imagebg cover cover-4 parallax" 
-      data-overlay="3">
-      <div className="background-image-holder background--bottom" style={imageHolderStyle}>
-        <img alt="image" src="/img/hero7.jpg"/>
-      </div>
-      <div className="container pos-vertical-center">
-        <div className="row">
-          <div className="col-sm-12 text-center">
-            <h1>
-              Digital, Delivered.
-              </h1>
-              <p className="lead">
-                  Beautiful front-end components
-                  <br/> built for modern startups.
-              </p>
-              <div>
-                <a className="btn" 
-                  href="#">
-                  <span className="btn__text">
-                    Purchase Now
-                  </span>
-                </a>
-                <div className="modal-instance modal-video-1">
-                  <div className="video-play-icon video-play-icon--sm modal-trigger" 
-                    data-modal-index="0"
-                  />
-                  <span className="h6">Watch Overview</span>
+interface Props {}
+interface State {
+  showVideo: boolean;
+}
+
+export default class StartupImage extends React.PureComponent <Props, State> {
+
+  constructor(props:Props) {
+    super(props);
+    this.state = {
+      showVideo: false,
+    };
+  }
+
+  handlePlay = () => this.setState({ showVideo:true });
+  handleStop = () => this.setState({ showVideo: false });
+
+  render() {
+
+    const { showVideo } = this.state;
+
+    const imageHolderStyle = {
+      transform: 'translate3d(0px, 12.5px, 0px)', 
+      background: 'url(img/hero7.jpg)', 
+      opacity: 1,
+      top: 0, 
+    };
+
+    return (
+      <section className="height-100 imagebg cover cover-4 parallax" 
+        data-overlay="3">
+        <div className="background-image-holder background--bottom" style={imageHolderStyle}>
+          <img alt="image" src="/img/hero7.jpg"/>
+        </div>
+        <div className="container pos-vertical-center">
+          <div className="row">
+            <div className="col-sm-12 text-center">
+              <h1>
+                Digital, Delivered.
+                </h1>
+                <p className="lead">
+                    Beautiful front-end components
+                    <br/> built for modern startups.
+                </p>
+                <div>
+                  <a className="btn" 
+                    href="#">
+                    <span className="btn__text">
+                      Purchase Now
+                    </span>
+                  </a>
+                  <div className="modal-instance modal-video-1">
+                    <div className="video-play-icon video-play-icon--sm modal-trigger" 
+                      data-modal-index="0"
+                      onClick={this.handlePlay}
+                    />
+                    <span className="h6">Watch Overview</span>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
-  );
-};
+          <YouTubeModal
+            src="https://www.youtube.com/embed/4QUVRm4UZA4?autoplay=1"
+            active={showVideo}
+            onClose={this.handleStop}
+          />
+        </section>
+    );
+  }
+}
