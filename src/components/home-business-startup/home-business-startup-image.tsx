76c7e32e9e3c5078a272cae28051e9eb77f7dae6
GaryGolf <garygolf@mail.ru>
2017-10-28 20:11:14 +0300

    add Parallax to home-business-startup header

--- src/components/home-business-startup/home-business-startup-image.tsx
+++ src/components/home-business-startup/home-business-startup-image.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import YouTubeModal from 'components/common/youtube-modal';
+import { Parallax } from 'react-parallax';
 
 interface Props {}
 interface State {
@@ -30,45 +31,44 @@ export default class StartupImage extends React.PureComponent <Props, State> {
     };
 
     return (
-      <section className="height-100 imagebg cover cover-4 parallax" 
-        data-overlay="3">
-        <div className="background-image-holder background--bottom" style={imageHolderStyle}>
-          <img alt="image" src="/img/hero7.jpg"/>
-        </div>
-        <div className="container pos-vertical-center">
-          <div className="row">
-            <div className="col-sm-12 text-center">
-              <h1>
-                Digital, Delivered.
-                </h1>
-                <p className="lead">
-                    Beautiful front-end components
-                    <br/> built for modern startups.
-                </p>
-                <div>
-                  <a className="btn" 
-                    href="#">
-                    <span className="btn__text">
-                      Purchase Now
-                    </span>
-                  </a>
-                  <div className="modal-instance modal-video-1">
-                    <div className="video-play-icon video-play-icon--sm modal-trigger" 
-                      data-modal-index="0"
-                      onClick={this.handlePlay}
-                    />
-                    <span className="h6">Watch Overview</span>
+      <Parallax bgImage="/img/hero7.jpg" strength={200}>
+        <section className="height-100 imagebg cover cover-4 parallax" 
+          data-overlay="3">
+          <div className="container pos-vertical-center">
+            <div className="row">
+              <div className="col-sm-12 text-center">
+                <h1>
+                  Digital, Delivered.
+                  </h1>
+                  <p className="lead">
+                      Beautiful front-end components
+                      <br/> built for modern startups.
+                  </p>
+                  <div>
+                    <a className="btn" 
+                      href="#">
+                      <span className="btn__text">
+                        Purchase Now
+                      </span>
+                    </a>
+                    <div className="modal-instance modal-video-1">
+                      <div className="video-play-icon video-play-icon--sm modal-trigger" 
+                        data-modal-index="0"
+                        onClick={this.handlePlay}
+                      />
+                      <span className="h6">Watch Overview</span>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <YouTubeModal
-            src="https://www.youtube.com/embed/4QUVRm4UZA4?autoplay=1"
-            active={showVideo}
-            onClose={this.handleStop}
-          />
-        </section>
+            <YouTubeModal
+              src="https://www.youtube.com/embed/4QUVRm4UZA4?autoplay=1"
+              active={showVideo}
+              onClose={this.handleStop}
+            />
+          </section>
+        </Parallax>
     );
   }
 }
